421c3bfa286f4fb216f72d25cdaf8e76d87a789e
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2023-06-11 01:50:17 -0300

    refatorando para padrão standard

--- hello-world/src/js/use-cases/distributeExpenses.js
+++ hello-world/src/js/use-cases/distributeExpenses.js
@@ -1,14 +1,14 @@
-import { round } from "../utils/round.js"
+import { round } from '../utils/round.js'
 
 export const distributeExpenses = (financialReleases, totalExpenses) => {
-    const expensesDistribution = []
-    for (const release of financialReleases) {
-        if (release.type === 'despesa') {
-            const percent = round((release.value / totalExpenses) * 100)
+  const expensesDistribution = []
+  for (const release of financialReleases) {
+    if (release.type === 'despesa') {
+      const percent = round((release.value / totalExpenses) * 100)
 
-            expensesDistribution.push({ category: release.category, percent })
-        }
+      expensesDistribution.push({ category: release.category, percent })
     }
+  }
 
-    return expensesDistribution
+  return expensesDistribution
 }
