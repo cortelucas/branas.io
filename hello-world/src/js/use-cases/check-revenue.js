5484a3c3f347e1bc99e6c4c5961a4c79dc5aedaa
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2023-06-14 16:29:09 -0300

    implementação do caso de uso de CheckRevenue

---
+++ hello-world/src/js/use-cases/check-revenue.js
@@ -0,0 +1,14 @@
+export class CheckRevenue {
+  constructor (month) {
+    this.month = month
+  }
+
+  execute () {
+    for (const release of this.month.financialReleases) {
+      if (release.type === 'receita') {
+        this.month.monthBalance.balance += release.value
+        this.month.monthBalance.revenues += release.value
+      }
+    }
+  }
+}
