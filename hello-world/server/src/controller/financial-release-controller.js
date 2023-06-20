a5ff480ae0a1913043a8b175bbb43c1c280c1534
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2023-06-20 16:31:09 -0300

    implementação do controller de lançamento

---
+++ hello-world/server/src/controller/financial-release-controller.js
@@ -0,0 +1,18 @@
+export class FinancialReleaseController {
+    constructor (httpServer, financialReleaseRepository) {
+        httpServer.register('get', '/api/financial-releases', async (params, body) => {
+            const financialReleases = await financialReleaseRepository.getFinancialReleases()
+            return financialReleases
+        })
+
+        httpServer.register('post','/api/financial-releases', async (params, body) => {
+            const financialRelease = body
+            await financialReleaseRepository.saveFinancialRelease(financialRelease)
+        })
+
+        httpServer.register('delete','/api/financial-releases/:id', async (params, body) => {
+            const id = params.id
+            await financialReleaseRepository.deleteFinancialRelease(id)
+        })
+    }
+}
