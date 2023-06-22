453ea2dcfe8046db21d6bb785cd4252b47971f8f
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2023-06-22 14:16:40 -0300

    Implementação da criação dinamica por meses

--- hello-world/client/src/js/use-cases/year/add-monthly-financial-release.js
+++ hello-world/client/src/js/use-cases/year/add-monthly-financial-release.js
@@ -1,4 +1,6 @@
+import { Month } from '../../domain/index.js'
 import { AddFinancialRelease } from '../month/index.js'
+import { AddMonthInYear } from './index.js'
 
 export class AddMonthlyFinancialRelease {
   constructor (year) {
@@ -6,6 +8,10 @@ export class AddMonthlyFinancialRelease {
   }
 
   execute (monthName, financialRelease) {
+    if (!this.year.months.some(month => month.name === monthName)) {
+      const addMonth = new AddMonthInYear(this.year)
+      addMonth.execute(new Month(monthName))
+    }
     for (const month of this.year.months) {
       if (month.name === monthName) {
         const addRelease = new AddFinancialRelease(month)
